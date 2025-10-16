7ec0bdbf47a84f3ba20ad7103b6d266739cb2ab5
Sultan11duck <152240797+Sultan11duck@users.noreply.github.com>
2025-10-16 19:59:43 +0300

    Update itunes.js

---
+++ js/itunes.js
@@ -0,0 +1,5 @@
+hostname = buy.itunes.apple.com
+https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://gist.githubusercontent.com/ddgksf2013/9e0f6c7341beea09a31aa309d9d7f502/raw/buyitunes.js
+https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/APPheji_iTunes.js
+https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/iTunes.js
